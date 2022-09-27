37a318379ef706304b724554d2881731098f9b21
Reema <Reemaalshohof88@gmail.com>
2022-09-27 12:37:58 +0200

    set up pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,13 @@
-import Landing from "./pages/Landing";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Landing, Dashboard, Register, Error } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      <nav>
-        <Link to="/">Dashboard</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/landing">landing</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
-        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="*" element={<h1>Error</h1>} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
