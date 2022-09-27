38f32321676023a6a16e1a2234cf1d5abb24941c
Reema <Reemaalshohof88@gmail.com>
2022-09-27 12:24:40 +0200

    add Link component to app and landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
     <Router>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/landing">landing</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<div>Dashboard</div>} />
         <Route path="/register" element={<div>Register</div>} />
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <Wrapper>
@@ -19,7 +20,9 @@ const Landing = () => {
             before they sold out banjo kale chips mlkshk taxidermy VHS godard
             poke four loko fashion axe.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="Job hunt" className="img main-img" />
       </div>
