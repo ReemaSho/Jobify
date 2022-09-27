d51a80c9ed855ad4f5739fb5570a3281f0b4bace
Reema <Reemaalshohof88@gmail.com>
2022-09-27 12:15:59 +0200

    set up the router react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import Landing from "./pages/Landing";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<h1>Error</h1>} />
+        <Route path="/" exact element={<div>Dashboard</div>} />
+      </Routes>
+    </Router>
   );
 };
 export default App;
